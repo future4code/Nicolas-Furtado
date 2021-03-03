64b5110235078eeab58b5807672887d5333a5481
Nicolas <nicolasalexandre0001@gmail.com>
2021-03-03 16:15:11 -0300

    parte do js pronta

---
+++ semana4/objetos/blog/main.js
@@ -0,0 +1,58 @@
+let posts = JSON.parse(document.cookie);
+
+const criarPost = () => {
+  const post = {
+    imagem: "",
+    titulo: "",
+    autor: "",
+    conteudo: "",
+  };
+  post.titulo = document.querySelector("#titulo-post").value;
+  post.autor = document.querySelector("#autor-post").value;
+  post.conteudo = document.querySelector("#conteudo-post").value;
+  post.imagem = document.querySelector("#imagem-post").value;
+  const imagem = document.createElement("img");
+  imagem.src = post.imagem;
+
+  if (
+    post.titulo != "" &&
+    post.autor != "" &&
+    post.conteudo != "" &&
+    post.imagem != ""
+  ) {
+    document.querySelector("#conteudo-post").value = "";
+    document.querySelector("#autor-post").value = "";
+    document.querySelector("#titulo-post").value = "";
+    document.querySelector("#imagem-post").value = "";
+    posts.push(post);
+
+    imagem.onerror = () => {
+      alert("Link inválido");
+      posts.splice(posts.length - 1, 1);
+      document.querySelector("#conteudo-post").value = post.conteudo;
+      document.querySelector("#autor-post").value = post.autor;
+      document.querySelector("#titulo-post").value = post.titulo;
+      document.querySelector("#imagem-post").value = post.imagem;
+    };
+  } else {
+    alert("Preencha todos os campos");
+  }
+  console.log(posts);
+  document.cookie = JSON.stringify(posts);
+};
+
+const renderizarPosts = () => {
+  posts = JSON.parse(document.cookie);
+  console.log(document.cookie);
+  for (post of posts){
+    document.querySelector("#container-de-posts").innerHTML += `
+      <div id='post'>
+        <img src ='${post.imagem}'>
+        <h4>${post.titulo}</h4>
+        <p>${post.conteudo}</p>
+        <span>autor:${post.autor}</span>
+      </div>
+    `;
+  }
+  
+}
