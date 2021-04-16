23a02932aab0f5b55340e1c0022ca222e67b66f3
Nicolas <nicolasalexandre0001@gmail.com>
2021-04-16 10:21:01 -0300

    feat - candidateForm

--- semana9/labex/src/components/CreateTripForm.js
+++ semana9/labex/src/components/CreateTripForm.js
@@ -1,17 +1,118 @@
-import React from "react";
+import { Button, MenuItem, Select, TextField } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 const MainContainer = styled.div`
   background-color: white;
   width: 90vw;
-  height: 300px;
   box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items:center;
+`;
+
+const StyledTextField = styled(TextField)`
+  margin: 5px;
+  width: 300px;
+`
+const StyledSelect = styled(Select)`
+  margin: 5px;
+  width: 300px;
+`
+const StyledButton = styled(Button)`
+  margin: 15px;
+  width: 100px;
+`
+
 const CreateTripForm = () => {
+  const [describe, setDescribe] = useState('');
+  const [name, setName] = useState("");
+  const [planet, setPlanet] = useState("");
+  const [date, setDate] = useState("");
+  const [duration, setDuration] = useState(0);
+
   return (
     <MainContainer>
-      <p>CreateTripForm</p>
+      <h3>Crie uma viagem</h3>
+      <FormContainer>
+        <div>
+          <StyledTextField
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+            variant="outlined"
+            label="Nome"
+          />
+          <StyledSelect
+            onChange={(e) => {
+              setPlanet(e.target.value);
+            }}
+            variant="outlined"
+            displayEmpty
+            value={planet}
+          >
+            <MenuItem value={''} disabled>
+              Planeta
+            </MenuItem>
+            <MenuItem value={"Vênus"}>Vênus</MenuItem>
+            <MenuItem value={"Terra"}>Terra</MenuItem>
+            <MenuItem value={"Marte"}>Marte</MenuItem>
+            <MenuItem value={"Plutão"}>Plutão</MenuItem>
+            <MenuItem value={"Netuno"}>Netuno</MenuItem>
+            <MenuItem value={"Saturno"}>Saturno</MenuItem>
+            <MenuItem value={"Mercúrio"}>Mercúrio</MenuItem>
+            <MenuItem value={"Urano"}>Urano</MenuItem>
+          </StyledSelect>
+          <StyledTextField
+            onChange={(e) => {
+              setDescribe(e.target.value);
+            }}
+            value={describe}
+            variant="outlined"
+            label="Descrição"
+          />
+        </div>
+        <div>
+          <StyledTextField
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+            value={date}
+            variant="outlined"
+            label="Data"
+          />
+          <StyledSelect
+            onChange={(e) => {
+              setDuration(e.target.value);
+            }}
+            variant="outlined"
+            displayEmpty
+            value={duration}
+          >
+            <MenuItem value={0} disabled>
+              Duração em dias
+            </MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value={40}>40</MenuItem>
+            <MenuItem value={50}>50</MenuItem>
+          </StyledSelect>
+        </div>
+
+        <StyledButton color="secondary" variant="outlined">
+          Criar
+        </StyledButton>
+      </FormContainer>
     </MainContainer>
   );
 };
