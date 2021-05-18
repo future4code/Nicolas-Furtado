b8c652394b235deb3f9e5ef64823504a7900c486
Nicolas <nicolasalexandre0001@gmail.com>
2021-05-18 15:54:31 -0300

    semana15-aula42

---
+++ semana15/aula42/src/exercicio4.ts
@@ -0,0 +1,37 @@
+/*
+a) Como você faria, já com a extensão instalada, para gerar um arquivo javascript a 
+partir do  arquivo typescript com o código abaixo?
+
+Usaria o comando tsc no terminal e usaria o caminho do arquivo como parametro
+
+
+
+
+
+b) E se este arquivo estivesse dentro de uma pasta chamada `src`. O processo 
+seria diferente? Se sim, descreva as diferenças.
+
+O processo poderia ser o mesmo, mas eu tambem poderia gerar um tsconfig.json com 
+tsc --init para automatizar a transpilação de todos os arquivos .ts de uma vez e 
+mandalos para uma pasta da minha preferencia.
+
+
+
+
+
+c) Existe alguma maneira de **transpilar** múltilplos arquivos de uma vez só? Caso conheça
+, explique como fazer.
+
+Poderia fazer isso configurando um arquivo tsconfig.json
+
+
+
+
+
+d) Compare esse arquivo com o que criamos durante a aula 
+(ele está disponível na área de configuração do projeto ali em cima).
+Leia as descrições sobre cada uma das propriedades. Alguma configuração 
+que chamou sua atenção? O que mudou em comparação com o arquivo criado pelos slides?
+
+O arquivo é bem maior e algumas configs foram descomentadas.
+*/
