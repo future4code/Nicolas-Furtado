bd73abfb813c22ca6471b32872da04d88e4e23ac
Nicolas <nicolasalexandre0001@gmail.com>
2021-06-11 14:24:53 -0300

    mvp

--- semana18/projeto/src/endpoints/getMyProfile.ts
+++ semana18/projeto/src/endpoints/getMyProfile.ts
@@ -1,17 +1,3 @@
-// Importar as coisas FEITO
-// Criar a base da requisição  FEITO
-// Exportar como default FEITO
-// Trycatch FEITO
-// Automatizar envio de erros FEITO
-
-// Verificar se o usuario ta logado FEITO
-// Pegar as informações do token (nome, email, id) FEITO
-// enviar informações
-
-// ATUALIZACAO
-// pegar user com o id do token no banco
-// enviar
-
 import { Response, Request } from "express";
 import { getTokenData } from "../middleware/authenticator";
 import connection from "../connection";
