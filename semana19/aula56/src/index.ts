d41722e517f780486dc4dac99ecc187e9bf93a0e
Nicolas <nicolasalexandre0001@gmail.com>
2021-06-15 13:59:35 -0300

    aula56

---
+++ semana19/aula56/src/index.ts
@@ -0,0 +1,7 @@
+import { app } from "./controller/app"
+import { taskRouter } from "./routes/taskRouter";
+import { userRouter } from "./routes/userRouter"
+
+app.use("/users", userRouter);
+app.use("/tasks", taskRouter)
+
